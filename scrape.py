9944291b4c21373bff0448c43e0fd07c38bb6bdf
Nick Swink <nickswink7@gmail.com>
2020-12-23 23:48:55 -0500

    flask api returns json with elements

--- scrape.py
+++ scrape.py
@@ -1,28 +1,52 @@
 
 import sys
 import json
+import flask
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
-# %%
-if __name__ == "__main__":
-    # %%
-    # url = 'https://www.newegg.com/black-purple-dowinx-ls-668806-computer-gaming/p/2T4-029X-00021?Item=9SIAKUYC2X4632&cm_sp=Homepage_SS-_-P0_9SIAKUYC2X4632-_-12222020&quicklink=true'
-    url = sys.argv[1]
+from flask import request, jsonify
+from flask_cors import CORS
+
+app = flask.Flask(__name__)
+app.config["DEBUG"] = True
+CORS(app)
+
+
+def get_product_elements(url):
     page = urlopen(url)
     html = page.read().decode("utf-8")
     soup = BeautifulSoup(html, "html.parser")
 
-    # %%
     product_title = soup.find('h1', class_='product-title')
     product_price_dollars = soup.find('li', class_='price-current').strong
     product_price_cents = soup.find('li', class_='price-current').sup
     product_ship = soup.find('div', class_='product-inventory').strong
-    # %%
+
     product_price = float(product_price_dollars.text) + \
         float(product_price_cents.text)
 
-    data = {"productTitle": product_title.text,
-            "productPrice": product_price,
-            "productAvailability": product_ship.text}
-    data_json = json.dumps(data)
-    print(data_json)
+    data = [{"productTitle": product_title.text,
+             "productPrice": product_price,
+             "productAvailability": product_ship.text}]
+    return data
+
+
+@app.route('/', methods=['GET'])
+def home():
+    message = []
+    message[0] = 'GET api/resources/products provide url returns productTitle, productPrice, productAvailability'
+    return message
+
+
+@app.route('/api/resources/products', methods=['GET'])
+def api_product():
+    if 'url' in request.args:
+        url = str(request.args['url'])
+    else:
+        return "Error: No url field provided. Please specify a url."
+
+    product_elements = get_product_elements(url)
+    return jsonify(product_elements)
+
+
+app.run()
