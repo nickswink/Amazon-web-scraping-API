1d9b423c255f34af3fe2d2cbdd6e08d721a94861
Nick Swink <nickswink7@gmail.com>
2020-12-23 22:05:10 -0500

    scraper collects cents now

--- scrape.py
+++ scrape.py
@@ -6,7 +6,7 @@ from urllib.request import urlopen
 # %%
 if __name__ == "__main__":
     # %%
-    #url = 'https://www.newegg.com/black-purple-dowinx-ls-668806-computer-gaming/p/2T4-029X-00021?Item=9SIAKUYC2X4632&cm_sp=Homepage_SS-_-P0_9SIAKUYC2X4632-_-12222020&quicklink=true'
+    # url = 'https://www.newegg.com/black-purple-dowinx-ls-668806-computer-gaming/p/2T4-029X-00021?Item=9SIAKUYC2X4632&cm_sp=Homepage_SS-_-P0_9SIAKUYC2X4632-_-12222020&quicklink=true'
     url = sys.argv[1]
     page = urlopen(url)
     html = page.read().decode("utf-8")
@@ -14,12 +14,15 @@ if __name__ == "__main__":
 
     # %%
     product_title = soup.find('h1', class_='product-title')
-    product_price = soup.find('li', class_='price-current').strong
+    product_price_dollars = soup.find('li', class_='price-current').strong
+    product_price_cents = soup.find('li', class_='price-current').sup
     product_ship = soup.find('div', class_='product-inventory').strong
-
     # %%
+    product_price = float(product_price_dollars.text) + \
+        float(product_price_cents.text)
+
     data = {"productTitle": product_title.text,
-            "productPrice": product_price.text,
+            "productPrice": product_price,
             "productAvailability": product_ship.text}
     data_json = json.dumps(data)
     print(data_json)
